f58ce9dc8bff2a9bbe7c37d38c0c3b3efff86175
Tom Gowan <s3549186@student.rmit.edu.au>
2018-12-13 13:17:52 +1100

    download and install

--- build.rs
+++ build.rs
@@ -1,10 +1,10 @@
 use std::env;
 use std::fs::File;
 use std::process::Command;
-use std::io::Write;
+use std::io::copy;
 use dirs;
-use curl::easy::Easy;
 use env_perm;
+use reqwest;
 
 fn main() {
     match env::var("VULKAN_SDK2") {
@@ -13,57 +13,67 @@ fn main() {
         // Install Vulkan SDK
         Err(_) =>(),
     }
-    {
-        let mut dl_dir = dirs::download_dir().expect("No download dir");
-        dl_dir.push("vulkansdk-macos-1.1.92.1.tar.gz");
-        let mut sdk = File::create(dl_dir).expect("Failed to create sdk tar");
-        let mut easy = Easy::new();
-        // TODO verify hash
-        easy.url("https://vulkan.lunarg.com/sdk/home#sdk/downloadConfirm/1.1.92.1/mac/vulkansdk-macos-1.1.92.1.tar.gz")
-            .expect("URL failed");
-        easy.write_function(move |data| {
-            sdk.write_all(data).expect("Failed to write");
-            Ok(data.len())
-        }).unwrap();
-        easy.perform().expect("Failed to perform curl");
+    let mut dl_dir = dirs::download_dir().expect("No download dir");
+    //let address = "https://sdk.lunarg.com/sdk/download/latest/mac/vulkan-sdk.tar.gz";
+    let address = "http://0.0.0.0:8000/vulkan-sdk.tar.gz";
+    let mut response = reqwest::get(address).expect("Failed to download sdk");
 
-        println!("{}", easy.response_code().unwrap());
+    let mut file_name: String;
+    {
+        let mut dest = {
+            let fname = response
+                .url()
+                .path_segments()
+                .and_then(|segments| segments.last())
+                .and_then(|name| if name.is_empty() { None } else { Some(name) })
+                .unwrap_or("vulkansdk.tar.gz");
+
+            eprintln!("file to download: '{}'", fname);
+            file_name = fname.to_string();
+            dl_dir.push(fname);
+            eprintln!("will be located under: '{:?}'", dl_dir);
+            File::create(&dl_dir).expect("failed to create file")
+        };
+        copy(&mut response, &mut dest).expect("Failed to write to file");
     }
 
-    Command::new("bash")
-            .arg("mkdir")
-            .arg("~/.vulkan_sdk")
-            .output()
-            .expect("failed to execute process");
-    
-    Command::new("bash")
-            .arg("mv")
-            .arg("~/Downloads/vulkansdk-macos-1.1.92.1.tar.gz ~/.vulkan_sdk")
-            .output()
-            .expect("failed to execute process");
-    
-    Command::new("bash")
-            .arg("cd")
-            .arg("~/.vulkan_sdk")
-            .arg("&&")
-            .arg("tar")
-            .arg("-xzf")
-            .arg("vulkansdk-macos-1.1.92.1.tar.gz")
-            .output()
-            .expect("failed to execute process");
 
-    Command::new("bash")
-            .arg("cd")
-            .arg("~/.vulkan_sdk")
-            .arg("&&")
-            .arg("mv")
-            .arg("-v")
-            .arg("vulkansdk-macos-1.1.92.1/* .")
-            .output()
-            .expect("failed to execute process");
+    let home = dirs::home_dir().expect("No home directory found");
+    let mut sdk_dir = home.clone();
+    sdk_dir.push(".vulkan_sdk");
     
+    let r = Command::new("mkdir")
+        .arg(&sdk_dir)
+        .output()
+        .expect("failed to execute process");
+    eprintln!("command: {:?}", r);
+
+    let r = Command::new("mv")
+        .arg(&dl_dir)
+        .arg(&sdk_dir)
+        .output()
+        .expect("failed to execute process");
+    eprintln!("command: {:?}", r);
+
+    let r = Command::new("tar")
+        .arg("-xzf")
+        .arg(format!("{}/{}", sdk_dir.display(), file_name))
+        .arg("-C")
+        .arg(&sdk_dir)
+        .arg("--strip-components=1")
+        .output()
+        .expect("failed to execute process");
+    eprintln!("command: {:?}", r);
+    let r = Command::new("rm")
+        .arg(format!("{}/{}", sdk_dir.display(), file_name))
+        .output()
+        .expect("failed to execute process");
+    eprintln!("command: {:?}", r);
+
+
+
     //export VULKAN_SDK=$HOME/vulkan_sdk/macOS
-    env_perm::check_or_set("VULKAN_SDK", r#""$HOME/vulkan_sdk/macOS""#).expect("Failed to set VULKAN_SDK");
+    env_perm::check_or_set("VULKAN_SDK", r#""$HOME/.vulkan_sdk/macOS""#).expect("Failed to set VULKAN_SDK");
     //export PATH=$VULKAN_SDK/bin:$PATH
     env_perm::append("PATH", "$VULKAN_SDK/bin").expect("Failed to append PATH");
     //export DYLD_LIBRARY_PATH=$VULKAN_SDK/lib:$DYLD_LIBRARY_PATH
